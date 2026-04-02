ab5e315053feac97aaba354c6a3624bc9a29d916
Georgey <gosha040105@gmail.com>
2026-04-02 21:18:25 +0300

    Бабенко Георгий Руслановивич dz1 baze

--- src/main/java/company/vk/edu/distrib/compute/Server.java
+++ src/main/java/company/vk/edu/distrib/compute/Server.java
@@ -1,17 +1,23 @@
 package company.vk.edu.distrib.compute;
 
-import module java.base;
-import company.vk.edu.distrib.compute.dummy.DummyKVServiceFactory;
+import java.io.IOException; // Добавлен необходимый импорт
+import company.vk.edu.distrib.compute.goshanchic.KVServiceFactoryImpl;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class Server {
 
-    void main() throws IOException {
-        var log = LoggerFactory.getLogger("server");
-        var port = 8080;
-        KVService storage = new DummyKVServiceFactory().create(port);
+    public static void main(String[] args) throws IOException { // Стандартное объявление main
+        Logger log = LoggerFactory.getLogger("server");
+        int port = 8080;
+
+        // Исправлено имя класса при создании объекта (убрана приставка 'goshanchic')
+        KVService storage = new KVServiceFactoryImpl().create(port);
+
         storage.start();
         log.info("Server started on port {}", port);
+
+        // Корректное завершение работы сервера при выключении приложения
         Runtime.getRuntime().addShutdownHook(new Thread(storage::stop));
     }
 }
